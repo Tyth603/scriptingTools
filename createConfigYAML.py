2c9da12296d9da7ee0b64761028b83238d1d6337
Tyth603 <chrisedavidson1@comcast.net>
2013-09-04 10:11:13 -0400

    now dealing with more unit-generator issues. specifically not correctly generating unit.xmls

--- createConfigYAML.py
+++ createConfigYAML.py
@@ -1,10 +1,12 @@
 import os
 from UnitObjectives import unitObjectives
+from LessonOrder import lessonOrder
 
 
 def createYAML(unitDir):
     configLocation = os.path.join(unitDir, "config.yaml")
     unit = os.path.split(os.path.split(unitDir)[0])[1]
+
     activities = ["Reading", "Preview", "SelfReportingRecognize", "Pronunciation", "AudioMultiChoice",
                   "MultipleChoice2", "Matching", "SelfReportingProduce", "Dictation2", "ProduceWritten"]
 
@@ -22,7 +24,8 @@ def createYAML(unitDir):
     Line7 = "  modules: %s \n" % modules
     Line8 = "  showhints: %s \n" % showHints
     Line9 = "description: '%s' \n" % description
-    lines = [Line1, Line2, Line3, Line4, Line5, Line6, Line7, Line8, Line9]
+    Line10 = "lessonOrder: '%s' \n" % lessonOrder
+    lines = [Line1, Line2, Line3, Line4, Line5, Line6, Line7, Line8, Line9, Line10]
     f = open(configLocation, "w+")
     for line in lines:
         f.write(line)
--- organizeB4Us.py
+++ organizeB4Us.py
@@ -1,8 +1,18 @@
 import os, sys, re, shutil, convertB4Us, createUnits, createConfigYAML
 
+
 class organizeB4U():
     def __init__(self):
-        self.proj1UnitNameList = ["Meeting and Greeting","Getting Help with your Language Learning","Getting More Help with your Language Learning","Polite Conversation","Shapes and Colors","Clothing","Shopping and Stores","Houses and Apartments","Rooms in a House","Family","Office and Professions","Parts of the Body","Emergencies","Places and Asking for Directions","School","Musical Instruments","Recreation","Nature","Animals","Food 1","Food 2","Going to a Restaurant","Languages","Countries","Travel","Buying Tickets","Taking a Taxi","Staying at a Hotel","Going to the Bank","Numbers","Days of the Week and Time","Seasons and Weather","Personal Pronouns and","Possessive Adjectives","Adjectives and Adverbs","Verbs","Prepositions"]
+        self.proj1UnitNameList = ["Meeting and Greeting", "Getting Help with your Language Learning",
+                                  "Getting More Help with your Language Learning", "Polite Conversation",
+                                  "Shapes and Colors", "Clothing", "Shopping and Stores", "Houses and Apartments",
+                                  "Rooms in a House", "Family", "Office and Professions", "Parts of the Body",
+                                  "Emergencies", "Places and Asking for Directions", "School", "Musical Instruments",
+                                  "Recreation", "Nature", "Animals", "Food 1", "Food 2", "Going to a Restaurant",
+                                  "Languages", "Countries", "Travel", "Buying Tickets", "Taking a Taxi",
+                                  "Staying at a Hotel", "Going to the Bank", "Numbers", "Days of the Week and Time",
+                                  "Seasons and Weather", "Personal Pronouns and", "Possessive Adjectives",
+                                  "Adjectives and Adverbs", "Verbs", "Prepositions"]
         self.languageDict = [
             ["x for English speakers", "Spanish"],
             ["x for English speakers", "Chinese, Mandarin"],
@@ -10,44 +20,47 @@ class organizeB4U():
             ["x for Chinese speakers", "English"],
             ["x for Spanish speakers", "English"],
             ["x for Indonesian Speakers", "English"],
-            ]
-        self.proj1UnitMap = {"Meeting and Greeting":["Meeting-and-Greeting"],
-            "Getting Help with your Language Learning": ["Communication-Facilitation"],
-            "Getting More Help with your Language Learning":["Helper-Relationship","Language-Learning-Facilitation","Translation-Facilitation"],
-            "Polite Conversation":["Polite-Conversation","Useful-Expressions"],
-            "Shapes and Colors":["Shapes","Colors"],
-            "Clothing":["Clothing"],
-            "Shopping and Stores":["Shopping"],
-            "Houses and Apartments":["House-Apartment"],
-            "Rooms in a House":["Living-Room","Dining-Room","Kitchen","Bathroom","Bedroom"],
-            "Family":["Family"],
-            "Office and Professions":["Office","Professions"],
-            "Parts of the Body":["Parts-of-the-Body"],
-            "Emergencies":["Emergencies"],
-            "Places and Asking for Directions":["Places","Asking-for-Directions"],
-            "School":["School"],
-            "Musical Instruments":["Musical-Instruments"],
-            "Recreation":["Recreation"],
-            "Nature":["Nature"],
-            "Animals":["Animals"],
-            "Food 1":["Meals","Beverages","Fruit","Vegetables","Grains"],
-            "Food 2":["Dairy","Meat","Seafood","Spices-Condiments","Dessert"],
-            "Going to a Restaurant":["At-the-Restaurant"],
-            "Languages":["Languages"],
-            "Countries":["Continents","Countries"],
-            "Travel":["Travel"],
-            "Buying Tickets":["Buying-Tickets"],
-            "Taking a Taxi":["Taking-a-Taxi"],
-            "Staying at a Hotel":["At-the-Hotel"],
-            "Going to the Bank":["Going-to-the-Bank"],
-            "Numbers":["Numbers-Cardinal","Numbers-Ordinal","Numbers-Other"],
-            "Days of the Week and Time":["Days-of-the-Week","Time","Asking-the-Time"],
-            "Seasons and Weather":["Months","Seasons","Weather"],
-            "Personal Pronouns and Possessive Adjectives":["Personal-Pronouns","Possessive-Adjectives","Conjunctions"],
-            "Adjectives and Adverbs":["Adjectives","Adverbs"],
-            "Verbs":["Verbs"],
-            "Prepositions":["Prepositions"],
-            }
+        ]
+        self.proj1UnitMap = {"Meeting and Greeting": ["Meeting-and-Greeting"],
+                             "Getting Help with your Language Learning": ["Communication-Facilitation"],
+                             "Getting More Help with your Language Learning": ["Helper-Relationship",
+                                                                               "Language-Learning-Facilitation",
+                                                                               "Translation-Facilitation"],
+                             "Polite Conversation": ["Polite-Conversation", "Useful-Expressions"],
+                             "Shapes and Colors": ["Shapes", "Colors"],
+                             "Clothing": ["Clothing"],
+                             "Shopping and Stores": ["Shopping"],
+                             "Houses and Apartments": ["House-Apartment"],
+                             "Rooms in a House": ["Living-Room", "Dining-Room", "Kitchen", "Bathroom", "Bedroom"],
+                             "Family": ["Family"],
+                             "Office and Professions": ["Office", "Professions"],
+                             "Parts of the Body": ["Parts-of-the-Body"],
+                             "Emergencies": ["Emergencies"],
+                             "Places and Asking for Directions": ["Places", "Asking-for-Directions"],
+                             "School": ["School"],
+                             "Musical Instruments": ["Musical-Instruments"],
+                             "Recreation": ["Recreation"],
+                             "Nature": ["Nature"],
+                             "Animals": ["Animals"],
+                             "Food 1": ["Meals", "Beverages", "Fruit", "Vegetables", "Grains"],
+                             "Food 2": ["Dairy", "Meat", "Seafood", "Spices-Condiments", "Dessert"],
+                             "Going to a Restaurant": ["At-the-Restaurant"],
+                             "Languages": ["Languages"],
+                             "Countries": ["Continents", "Countries"],
+                             "Travel": ["Travel"],
+                             "Buying Tickets": ["Buying-Tickets"],
+                             "Taking a Taxi": ["Taking-a-Taxi"],
+                             "Staying at a Hotel": ["At-the-Hotel"],
+                             "Going to the Bank": ["Going-to-the-Bank"],
+                             "Numbers": ["Numbers-Cardinal", "Numbers-Ordinal", "Numbers-Other"],
+                             "Days of the Week and Time": ["Days-of-the-Week", "Time", "Asking-the-Time"],
+                             "Seasons and Weather": ["Months", "Seasons", "Weather"],
+                             "Personal Pronouns and Possessive Adjectives": ["Personal-Pronouns",
+                                                                             "Possessive-Adjectives", "Conjunctions"],
+                             "Adjectives and Adverbs": ["Adjectives", "Adverbs"],
+                             "Verbs": ["Verbs"],
+                             "Prepositions": ["Prepositions"],
+        }
         self.pathNames = []
         self.pathName = os.path.abspath(os.path.dirname(sys.argv[0]))
         self.proj1Dir = os.path.join(self.pathName, "project1")
@@ -71,8 +84,7 @@ class organizeB4U():
                     self.getProjLists(path, newPath)
 
 
-
-    def areaPrep(self, fullPath):    
+    def areaPrep(self, fullPath):
         if os.path.exists(fullPath):
             pass
         else:
@@ -88,16 +100,13 @@ class organizeB4U():
                 self.pathNames.append(languagePath)
 
 
-
-    
     def createDir(self, fullPath):
         try:
             os.makedirs(fullPath)
         except IOError as e:
-            print "I/O error({0}): {1}".format(e.errno, e.strerror)    
-        
-    
-    
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+
+
     def getProjLists(self, initialDir, finalDir):
         for directory, dirnames, filenames in os.walk(initialDir):
             for file in filenames:
@@ -109,18 +118,18 @@ class organizeB4U():
                         if os.path.exists(os.path.split(finalPath)[0]):
                             shutil.copyfile(originalPath, finalPath)
                         else:
-                             os.mkdir(os.path.split(finalPath)[0])
-                             shutil.copyfile(originalPath, finalPath)
+                            os.mkdir(os.path.split(finalPath)[0])
+                            shutil.copyfile(originalPath, finalPath)
 
-                    # if self.unitNameCheck(file, self.proj1UnitNameList) != None:
-                    #     unitName = self.unitNameCheck(file, self.proj1UnitNameList)
-                    #     finalPath = os.path.join(finalDir, unitName, file)
-                    #     originalPath = os.path.join(directory, file)
-                    #     if os.path.exists(os.path.split(finalPath)[0]):
-                    #         shutil.copyfile(originalPath, finalPath)
-                    #     else:
-                    #         os.mkdir(os.path.split(finalPath)[0])
-                    #         shutil.copyfile(originalPath, finalPath)
+                            # if self.unitNameCheck(file, self.proj1UnitNameList) != None:
+                            #     unitName = self.unitNameCheck(file, self.proj1UnitNameList)
+                            #     finalPath = os.path.join(finalDir, unitName, file)
+                            #     originalPath = os.path.join(directory, file)
+                            #     if os.path.exists(os.path.split(finalPath)[0]):
+                            #         shutil.copyfile(originalPath, finalPath)
+                            #     else:
+                            #         os.mkdir(os.path.split(finalPath)[0])
+                            #         shutil.copyfile(originalPath, finalPath)
 
     def unitNameMapCheck(self, fileName, unitMap):
         for unitName in unitMap.keys():
@@ -135,10 +144,10 @@ class organizeB4U():
                 return unitName
             else:
                 pass
-    
+
     def moveLists(self, file, projDir):
         langDir = os.path.join(projDir, os.path.split(os.path.split(file)[0])[1])
-        finalDir = os.path.join(projDir,os.path.split(os.path.split(file)[0])[1], os.path.split(file)[1])
+        finalDir = os.path.join(projDir, os.path.split(os.path.split(file)[0])[1], os.path.split(file)[1])
         if not os.path.exists(os.path.join(projDir, os.path.split(file)[1])):
             if os.path.exists(langDir):
                 try:
@@ -154,6 +163,7 @@ class organizeB4U():
                 except:
                     print "Move List Error"
         return finalDir
+
 
 if __name__ == "__main__":
     organizeB4U()
