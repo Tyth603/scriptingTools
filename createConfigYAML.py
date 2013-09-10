347134b1357e6a41522c02ae65d00d64b9886077
Tyth603 <chrisedavidson1@comcast.net>
2013-09-10 16:08:32 -0400

    working to fix the issue with ESL courses and folder name violations in Windows

--- createConfigYAML.py
+++ createConfigYAML.py
@@ -35,8 +35,8 @@ def createYAML(course):
     Line7 = "  modules: %s \n" % modules
     Line8 = "  showhints: %s \n" % showHints
     Line9 = "description: '%s' \n" % description
-    Line10 = u"lessonOrder: %s \n" % lessonOrder[unit]
-    Line11 = u"lessonListMap: %s \n" % lessonMapString
+    Line10 = u"lessonOrder: %s \n" % unicode(lessonOrder[unit])
+    Line11 = u"lessonListMap: %s \n" % unicode(lessonMapString)
     Line12 = "isESLTrue: %s \n" % str(course.isESLTrue)
     lines = [Line1, Line2, Line3, Line4, Line5, Line6, Line7, Line8, Line9, Line10, Line11, Line12]
     f = codecs.open(configLocation, "w+", "utf-8")
