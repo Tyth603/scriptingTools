7dfdd3f49fa13363b3ff6e87e9f495e6bad0cdab
Tyth603 <chrisedavidson1@comcast.net>
2013-09-10 16:44:33 -0400

    working to fix the issue with ESL courses and folder name violations in Windows

--- QuickStartCourses.py
+++ QuickStartCourses.py
@@ -43,51 +43,25 @@ unitMap = {
         "10-Quick-Start",
     ]
 }
-englishForIndonesianUnitObjectives = {"Start Cepat Unit 1": """Unit ini mengajarkan kata-kata dan frasa-frasa penting yang terkait dengan sapaan umum, frasa dasar yang sopan, tempat-tempat sekitar, dan menyewa kamar di hotel.
-
+baseText = """
 Unit ini akan membantu Anda...
 
 - mengingat kata-kata dan frasa-frasa yang terkait dengan topik ini
 - belajar membaca dan menulis kata-kata dan frasa-frasa tersebut
 - mengenal bunyi dan bisa mengucapkannya dengan benar
 
-Mulailah dengan aktivitas seperti Perbandingan Bahasa dan Latihan Pengucapan untuk membantu Anda menjadi terbiasa dengan materi ini.
+Mulailah dengan aktivitas seperti Language Comparison (Perbandingan Bahasa) dan Pronunciation Practice (Latihan Pengucapan) untuk membantu Anda menjadi terbiasa dengan materi ini.
 
 Lanjutkan dengan aktivitas pilihan ganda dan menjodohkan yang berbasis wicara untuk meningkatkan pemahaman Anda atas kata-kata dan frasa-frasa yang diberikan.
 
 Kemudian, lengkapi aktivitas menyimak dan mengetik untuk mempraktikkan produksi dan penulisan bahasa.
 
-Akhirnya, lengkapi aktivitas penilaian pada akhir unit untuk menguji pengetahuan Anda atas kata-kata dan frasa-frasa yang diberikan.""",
-                                      "Start Cepat Unit 2": """Unit ini mengajarkan kata-kata dan frasa-frasa penting yang terkait dengan berbelanja di toko atau pasar, memesan di restoran, hari-hari dalam seminggu, cuaca, dan waktu.
+Akhirnya, lengkapi aktivitas penilaian pada akhir unit untuk menguji pengetahuan Anda atas kata-kata dan frasa-frasa yang diberikan."""
 
-Unit ini akan membantu Anda...
-
-- mengingat kata-kata dan frasa-frasa yang terkait dengan topik ini
-- belajar membaca dan menulis kata-kata dan frasa-frasa tersebut
-- mengenal bunyi dan bisa mengucapkannya dengan benar
-
-Mulailah dengan aktivitas seperti Perbandingan Bahasa dan Latihan Pengucapan untuk membantu Anda menjadi terbiasa dengan materi ini.
-
-Lanjutkan dengan aktivitas pilihan ganda dan menjodohkan yang berbasis wicara untuk meningkatkan pemahaman Anda atas kata-kata dan frasa-frasa yang diberikan.
-
-Kemudian, lengkapi aktivitas menyimak dan mengetik untuk mempraktikkan produksi dan penulisan bahasa.
-
-Akhirnya, lengkapi aktivitas penilaian pada akhir unit untuk menguji pengetahuan Anda atas kata-kata dan frasa-frasa yang diberikan.""",
-                                      "Start Cepat Unit 3": """Unit ini mengajarkan kata-kata dan frasa-frasa penting yang terkait dengan keterampilan bahasa Inggris, memberikan arah ke suatu lokasi, dan meminta bantuan dalam situasi darurat.
-
-Unit ini akan membantu Anda...
-
-- mengingat kata-kata dan frasa-frasa yang terkait dengan topik ini
-- belajar membaca dan menulis kata-kata dan frasa-frasa tersebut
-- mengenal bunyi dan bisa mengucapkannya dengan benar
-
-Mulailah dengan aktivitas seperti Perbandingan Bahasa dan Latihan Pengucapan untuk membantu Anda menjadi terbiasa dengan materi ini.
-
-Lanjutkan dengan aktivitas pilihan ganda dan menjodohkan yang berbasis wicara untuk meningkatkan pemahaman Anda atas kata-kata dan frasa-frasa yang diberikan.
-
-Kemudian, lengkapi aktivitas menyimak dan mengetik untuk mempraktikkan produksi dan penulisan bahasa.
-
-Akhirnya, lengkapi aktivitas penilaian pada akhir unit untuk menguji pengetahuan Anda atas kata-kata dan frasa-frasa yang diberikan.""", }
+englishForIndonesianUnitObjectives = {"Start Cepat Unit 1": "Unit ini mengajarkan kata-kata dan frasa-frasa penting yang terkait dengan sapaan umum, frasa dasar yang sopan, tempat-tempat sekitar, dan menyewa kamar di hotel." + "\n" + baseText,
+"Start Cepat Unit 2": "Unit ini mengajarkan kata-kata dan frasa-frasa penting yang terkait dengan berbelanja di toko atau pasar, memesan di restoran, hari-hari dalam seminggu, cuaca, dan waktu." + "\n" + baseText,
+"Start Cepat Unit 3": "Unit ini mengajarkan kata-kata dan frasa-frasa penting yang terkait dengan keterampilan bahasa Inggris, memberikan arah ke suatu lokasi, dan meminta bantuan dalam situasi darurat." + "\n" + baseText,
+}
 
 unitNumbers = {"Start Cepat Unit 1": 1,
                "Start Cepat Unit 2": 2,
